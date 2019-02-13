57568f8d9efbbd3145ef606df5f048fd343b3a9f
dezhizhang <1541609448@qq.com>
2019-02-13 21:40:21 +0800

    修改请求

--- config/plugin.js
+++ config/plugin.js
@@ -2,3 +2,17 @@
 
 // had enabled by egg
 // exports.static = true;
+
+//使用mongodb数据库
+exports.mongoose = {
+    enable: true,
+    package: 'egg-mongoose',
+  };
+
+
+
+exports.cors = {
+    enable: true,
+    package: 'egg-cors',
+};
+
